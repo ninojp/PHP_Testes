aaf4584c9bb6bc5ab5e36cf7be8422dc11171f7c
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-10-08 17:03:38 -0300

    mudando as estruturas

--- public/index.php
+++ public/index.php
@@ -1,27 +1,4 @@
-<?php
-session_start();
-$_SESSION['nova_session'] = 'Conteudo da Sessao de Teste';
-// testando o uso de cookies 
-$nome_cookie = "teste_index";
-$valor_cookie = "user_logado";
-$tempo_expirar = time() + (1 * 24 * 3600); # Tempo de validade do COOKIE, +(1*24*3600) 1dia x 24horas x 3600segundos
-$path = "C:\\xampp\\htdocs\\php_testes\\public"; #Só pode ser acessado por este diretório
-$domain = "localhost"; #Só pode ser acessado por este Domínio
-$secure = false; # true - Só pode ser acessado via HTTPS
-$httponly = true; # true - para indicar que NÃO pode ser acessado via JavaScript
-
-setcookie($nome_cookie, $valor_cookie, $tempo_expirar, $path, $domain, $secure, $httponly = false);
-header("Set-cookie: $nome_cookie=$valor_cookie; SameSite=Lax; Secure");
-
-//definindo as variaveis e o array para obter a DATA atual
-$dia = date("d");
-$mes = date("m");
-$ano = date("Y"); //y minusculo para ano com dois algarismos
-$meses = array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
-//definindo as variaveis para obter a HORA atual
-$hora = date("H"); //H maiusculo para o formato 24h
-$minuto = date("i");
-$segundo = date("s");?>
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -34,25 +11,9 @@ $segundo = date("s");?>
     <title>Aprendendo PHP</title>
 </head>
 <body>
-    <div class="menu_lateral">
-        <ul>
-            <li>Material de Referência</li>
-            <li><a href="https://www.php.net/manual/pt_BR/index.php" target="blank">Manual do PHP</a></li>
-            <li><a href="https://www.w3schools.com/php/php_intro.asp" target="blank">W3schools PHP</a></li>
-            <li><a href="" target="blank">João Ribeiro PHP</a>
-                <ul class="ul_sub">
-                    <li><a href="https://www.youtube.com/watch?v=krQj4stNu5U&list=PLXik_5Br-zO_5EGPG6_u-u0hVI_f_ThO_" target="blank">PHP Boas Práticas 2017</a><li>
-                    <li><a href="https://www.youtube.com/watch?v=jVUeF7cZdFE&list=PLXik_5Br-zO9wODVI0j58VuZXkITMf7gZ" target="blank">PHP Curso Completo 2020</a><li>
-                    <li><a href="https://www.youtube.com/watch?v=iDEuQjITm0E&list=PLXik_5Br-zO9Z8l3CE8zaIBkVWjHOboeL" target="blank">Curso Completo Parte2 2021</a><li>
-                    <li><a href="https://www.youtube.com/watch?v=OMn8y1RE4FE&list=PLXik_5Br-zO-tsUy1lTPB8dnPGBu8n0Ee&index=3" target="blank">PHP Loja Virtual</a></li>
-                    <li><a href="https://www.youtube.com/watch?v=gsPp71nRMQk&list=PLXik_5Br-zO8vLD6X9uB-EH6BpgZL8XBH" target="blank">PHP APIs</a></li>
-                </ul></li>
-            <li><a href="https://www.youtube.com/watch?v=F7KzJ7e6EAc&list=PLHz_AreHm4dm4beCCCmW4xwpmLf6EHY9k&index=1" target="blank">PHP Curso em Video 2014</a></li>
-            <li><a href="https://www.youtube.com/watch?v=R2KdI-DDYCc&list=PLnex8IkmReXw-QlzKS9zA3rXQsRnK5nnA&index=3" target="blank">PHP7 Edson Maia PHP 2021</a></li>
-            <li><a href="https://www.youtube.com/watch?v=71dz_gaE798&list=PLx4x_zx8csUgB4R1dDXke4uKMq-IrSr4B&index=3" target="blank">PHP CFBCursos 2014</a></li>
-            <li><a href="https://celke.com.br/clms/home/index" target="blank">Celke PHP Development</a></li>
-        </ul>
-    </div>
+    <?php require_once "nav_top.php";
+            include_once "header.php";?>
+    
     <header class="header_nav container-fluid">
         <div class="div_cookie text_peq">
             <?php if (isset($_COOKIE['teste_index'])){echo"Cookie de Teste: ".$_COOKIE['teste_index'];}?>
--- public/testando.php
+++ public/testando.php
@@ -9,37 +9,62 @@
 </head>
 <body>
     <?php 
-        if (isset($_GET['v1'])) {
-            $v_valor1=$_GET['v1'];
-        }
-        if (isset($_GET['v2'])) {
-            $v_valor2=$_GET['v2'];
-        } 
+        # Recebendo os dados do FORMULÁRIO via _POST
+        if (isset($_POST['input_words'])) {$v_string=$_POST['input_words'];} 
+        if (isset($_POST['input_number_1'])) {$v_number1=$_POST['input_number_1'];} 
+        if (isset($_POST['input_number_2'])) {$v_number2=$_POST['input_number_2'];} 
+        if (isset($_POST['input_email'])) {$v_email=$_POST['input_email'];} 
+        if (isset($_POST['input_password'])) {$v_password=$_POST['input_password'];} 
     ?>
-    <div class="container text-center">
+    <div class="container-fluid ps-5 pe-5">
         <div class="row justify-content-center">
-            <h1 class="alert alert-success">Bora Praticar</h1>
-            <div class="col-5 alert alert-primary">
-            <h2>Valores recebidos via url:</h2>
-                <?php if(isset($v_valor1)){
-                    echo "O valor da variavel 1 é: $v_valor1 <br>";
-                }else{
-                    echo "Variavel V1 não definida <br>";
-                }
-                if(isset($v_valor2)){
-                    echo "O valor da variavel 2 é: $v_valor2 <br>";
-                }else{
-                    echo "Variavel V2 não definida <br>";
-                }?>
+            <h1 class="alert alert-primary text-center">Bora Praticar PHP</h1>
+            <div class="col-4 alert alert-primary">
+            <h2 class="text-center">Formulário para inserção</h2>
+            <form class="p-3" method="POST" name="form_insert" action="">
+                <div class="mb-3">
+                    <label class="form-label mb-0 pb-0" for="input_words">Inserir Palavras:</label>
+                    <input id="input_words" name="input_words" class="form-control" type="text" placeholder="Inserir Palavras (Strings)">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label mb-0 pb-0" for="input_number_1">Insira o valor 1:</label>
+                    <input id="input_number_1" name="input_number_1" class="form-control" type="number" placeholder="Insira o valor 1 (Number)">
+                    <label class="form-label mb-0 pb-0" for="input_number_2">Insira o valor 2:</label>
+                    <input id="input_number_2" name="input_number_2" class="form-control" type="number" placeholder="Insira o valor 2 (Number)">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label mb-0 pb-0" for="input_email">Insira o E-mail:</label>
+                    <input id="input_email" name="input_email" class="form-control" type="email" placeholder="Insira o E-mail">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label mb-0 pb-0" for="input_password">Insira a Senha:</label>
+                    <input id="input_password" name="input_password" class="form-control" type="password" placeholder="Insira a Senha">
+                </div>
+                <div class="row justify-content-between">
+                    <div class="mt-4 col-4">
+                        <button class="btn btn-warning" type="reset">Limpar Form</button>
+                    </div>
+                    <div class="mt-4 col-3">
+                        <button class="btn btn-primary" type="submit">Enviar</button>
+                    </div>
+                </div>
+            </form>
             </div>
-            <div class="col-5 alert alert-info">
-            <h2>Exibindo a variavel recebida via url:</h2>
-                <?php if(isset($v_valor1) || ($v_valor2)){
-                    $v_soma= $v_valor1 + $v_valor2;
+            <div class="col-4 alert alert-success">
+                <?php # Verificando e exibindo os dados recebidos do formulario
+                echo "<h3>Exibindo as variaveis do formulário:</h3><br>";
+                if(isset($v_string)){echo "O Texto é: $v_string<br>";}
+                if(isset($v_number1)){echo "O Primeiro numero é: $v_number1<br>";}
+                if(isset($v_number2)){echo "O Segundo numero é: $v_number2<br>";}
+                if(isset($v_email)){echo "O E-mail é: $v_email<br>";}
+                if(isset($v_password)){echo "A Senha é: $v_password<br>";}
+                ?>
+            </div>
+            <div class="col-4 alert alert-info">
+                <h4>Trabalhando com dados vindos do Furmulário:</h4>
+                <?php if(isset($v_number1) and ($v_number2)){$v_soma= $v_number1 + $v_number2;
                     echo "A Soma de V1 + V2 é: $v_soma <br>";
-                }else{
-                    echo "Valor não definido!";
-                }?>
+                }else{echo "Valor não definido!";}?>
             </div>
         </div>
     </div>
